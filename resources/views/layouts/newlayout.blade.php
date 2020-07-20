e3b15b1fbc92c06593373f1a237348c0bae73286
Dyony <dyony_rezende@live.com>
2020-07-20 13:53:53 -0300

    content for layout view

---
+++ resources/views/layouts/newlayout.blade.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<link rel="stylesheet" href="{{asset('css/app.css')}}">
+	<title>Validations</title>
+</head>
+
+<body>
+	<div class="container">
+		<main role="main">
+			@hassection('body')
+			@yield('body')
+			@endif
+		</main>
+	</div>
+	<script src="{{'asset(js/app.js)'}}" type="text/javascript"></script>
+	@hasSection('javascript') 
+	@yield('javascript') 
+	@endif
+	
+</body>
+	
+</html>
